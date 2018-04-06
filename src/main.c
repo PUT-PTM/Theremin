ef6b97487fa3f6777d9698e3e102c02da24643f5
wydzik <lukasz.dawydzik@student.put.poznan.pl>
2018-04-06 16:02:59 +0200

    Cyclic measurement, no data interpreted

--- src/main.c
+++ src/main.c
@@ -47,45 +47,49 @@ int main(void) {
 //	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure1);
 
 
-	GPIO_SetBits(GPIOD, GPIO_Pin_12);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+//10 us
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_TimeBaseStructure.TIM_Period = 9;
+	TIM_TimeBaseStructure.TIM_Prescaler = 83;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+//Trigger
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+//Echo	
+	GPIO_InitTypeDef GPIO_InitStructure1;
+	GPIO_InitStructure1.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure1.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure1.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure1.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure1.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStructure1);
 
-	while (1) {
-		if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13) == 1) {
-			GPIO_SetBits(GPIOD, GPIO_Pin_14);
-		} else {
-			GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+	TIM_Cmd(TIM4, ENABLE);
+//Cykliczne pomiary
+	for (;;) {
+		if (TIM_GetFlagStatus(TIM4, TIM_FLAG_Update)) {
+			GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+			if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_12) == 1) {
+				a = 1;
+			} else {
+				a = 0;
+			}
+			if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13) == 1) {
+				b = 1;
+			} else {
+				b = 0;
+			}
+			TIM_ClearFlag(TIM4, TIM_FLAG_Update);
 		}
+
 	}
-//	// Konfiguracja pinów
-//
-//	// Ustawiamy TRIG w stan niski na 2us
-//	GPIO_SetBits(GPIOD, GPIO_Pin_13);
-//
-//	// Ustawiamy TRIG w stan wysoki na 10us
-//
-//
-//
-//	while (1) {
-//
-//
-////		// Ustawiamy TRIG w stan niski - rozpoczynamy pomiar
-////		GPIO_ResetBits(GPIOD, GPIO_Pin_12);
-////
-////		// Odczytujamy czas trwania stanu wysokiego na pinie ECHO
-////		EchoTime = pulseIn(GPIOD, GPIO_Pin_0, 1);
-////
-////		// Obliczamy odległość
-////		Distance = EchoTime / 58;
-////
-////		// Sprawdzamy zakres pomiarowy
-////		if (Distance >= MaximumRange || Distance <= MinimumRange) {
-////			printf("Poza zakresem");
-////		} else {
-////			printf("Odleglosc: ", Distance);
-////		}
-////
-////		// Opóźnienie kolejnego pomiaru
-////		for (int i = 0; i = 1000; i++)
-////			;
-//	}
 }
