92b81d627f3ce4366fad64c042b8c7f61ab1cf7f
Łukasz Dawydzik <lukasz.dawydzik@student.put.poznan.pl>
2018-04-23 21:19:49 +0200

    powinno działać, ale nie działa

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@ int b;
 float Distance;
 unsigned int TIM2_CNT;
 unsigned int TIM4_CNT;
-//Na razie odstawione, bo dzialalo bez dodatkowego timera, testy w najprostszych warunkach
+
+
+//Na razie odstawione, testy w najprostszych warunkach
 //void TIM4_IRQHandler(void) {
 //
 //	if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET) {
@@ -33,6 +35,12 @@ unsigned int TIM4_CNT;
 //	}
 //}
 
+void Delay(uint32_t i) {
+	static uint32_t ij = 0, j = 0;
+	for (ij = 0; ij < i; ij++)
+		for (j = 0; j < 1; j++)
+			;
+}
 
 int main(void) {
 
@@ -52,28 +60,28 @@ int main(void) {
 	GPIO_InitStructure1.GPIO_Mode = GPIO_Mode_IN;
 	GPIO_InitStructure1.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure1.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure1.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure1.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_Init(GPIOD, &GPIO_InitStructure1);
 
 	/////DRUGI
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+//	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
 //Trigger, podlaczony poki co do zielonej diody, zeby ladnie bylo widac czy idzie sygnal wysoki, czy nie.
-	GPIO_InitTypeDef GPIO_InitStructure3;
-	GPIO_InitStructure3.GPIO_Pin = GPIO_Pin_8;
-	GPIO_InitStructure3.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure3.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure3.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure3.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOC, &GPIO_InitStructure3);
-// Echo, rowniez podlaczone do diody, w tym samym celu.
-	GPIO_InitTypeDef GPIO_InitStructure4;
-	GPIO_InitStructure4.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStructure4.GPIO_Mode = GPIO_Mode_IN;
-	GPIO_InitStructure4.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure4.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure4.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOC, &GPIO_InitStructure4);
+//	GPIO_InitTypeDef GPIO_InitStructure3;
+//	GPIO_InitStructure3.GPIO_Pin = GPIO_Pin_8;
+//	GPIO_InitStructure3.GPIO_Mode = GPIO_Mode_OUT;
+//	GPIO_InitStructure3.GPIO_OType = GPIO_OType_PP;
+//	GPIO_InitStructure3.GPIO_Speed = GPIO_Speed_100MHz;
+//	GPIO_InitStructure3.GPIO_PuPd = GPIO_PuPd_NOPULL;
+//	GPIO_Init(GPIOC, &GPIO_InitStructure3);
+//// Echo, rowniez podlaczone do diody, w tym samym celu.
+//	GPIO_InitTypeDef GPIO_InitStructure4;
+//	GPIO_InitStructure4.GPIO_Pin = GPIO_Pin_9;
+//	GPIO_InitStructure4.GPIO_Mode = GPIO_Mode_IN;
+//	GPIO_InitStructure4.GPIO_OType = GPIO_OType_PP;
+//	GPIO_InitStructure4.GPIO_Speed = GPIO_Speed_100MHz;
+//	GPIO_InitStructure4.GPIO_PuPd = GPIO_PuPd_NOPULL;
+//	GPIO_Init(GPIOC, &GPIO_InitStructure4);
 
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure9;
@@ -86,8 +94,8 @@ int main(void) {
 // Timer4, czestotliwosc 100Hz wydaje sie byc najbardziej przyjazdna dla czujnika. Maksymalna czestotliwosc to 100kHz.
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-	TIM_TimeBaseStructure.TIM_Period = 999;
-	TIM_TimeBaseStructure.TIM_Prescaler = 839;
+	TIM_TimeBaseStructure.TIM_Period = 9999;
+	TIM_TimeBaseStructure.TIM_Prescaler = 8399;
 	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
@@ -103,51 +111,92 @@ int main(void) {
 
 // Timer 3, czestotliwosc 100Hz wydaje sie byc najbardziej przyjazdna dla czujnika. Maksymalna czestotliwosc to 100kHz.
 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure1;
-	TIM_TimeBaseStructure1.TIM_Period = 999;
-	TIM_TimeBaseStructure1.TIM_Prescaler = 839;
-	TIM_TimeBaseStructure1.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure1.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure1);
+//	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+//	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure1;
+//	TIM_TimeBaseStructure1.TIM_Period = 999;
+//	TIM_TimeBaseStructure1.TIM_Prescaler = 839;
+//	TIM_TimeBaseStructure1.TIM_ClockDivision = TIM_CKD_DIV1;
+//	TIM_TimeBaseStructure1.TIM_CounterMode = TIM_CounterMode_Up;
+//	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure1);
+//
+//	NVIC_InitTypeDef NVIC_InitStructure1;
+//	NVIC_InitStructure1.NVIC_IRQChannel = TIM3_IRQn;
+//	NVIC_InitStructure1.NVIC_IRQChannelPreemptionPriority = 0x00;
+//	NVIC_InitStructure1.NVIC_IRQChannelSubPriority = 0x00;
+//	NVIC_InitStructure1.NVIC_IRQChannelCmd = ENABLE;
+//	NVIC_Init(&NVIC_InitStructure1);
+//	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+//	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
 
-	NVIC_InitTypeDef NVIC_InitStructure1;
-	NVIC_InitStructure1.NVIC_IRQChannel = TIM3_IRQn;
-	NVIC_InitStructure1.NVIC_IRQChannelPreemptionPriority = 0x00;
-	NVIC_InitStructure1.NVIC_IRQChannelSubPriority = 0x00;
-	NVIC_InitStructure1.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure1);
-	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
 
-	TIM_Cmd(TIM4, ENABLE);
 //	TIM_Cmd(TIM3, ENABLE);
-	TIM_Cmd(TIM2, DISABLE);
+	//TIM_Cmd(TIM2, DISABLE);
+//	for (b = 0; b < 10; b++) {
+//		GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+//		TIM_Cmd(TIM2, ENABLE);
+//		if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13)) {
+//			TIM2_CNT = TIM2->CNT;
+//			TIM_Cmd(TIM2, DISABLE);
+//		}
+//		for (a = 0; a < 1000000; a++)
+//		;
+//			GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+//		for (a = 0; a < 1000000; a++)
+//			;
+//	}
+	//	TIM_Cmd(TIM2, ENABLE);
+//	if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13)) {
+//		TIM2_CNT = TIM2->CNT;
+//
+//	} else if (!GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13)) {
+//		//Odczytanie wartosci licznika i pomiar
+//
+//		//Distance = (float) TIM2_CNT / 58;
+//		//TIM_Cmd(TIM2, DISABLE);
+//				}
 
+//	for (;;) {
+//		if (TIM_GetFlagStatus(TIM4, TIM_FLAG_Update)) {
+////Wyslany sygnal trigger
+//			GPIOD->BSRRH = GPIO_Pin_12;
+//			Delay(1000000);
+//			GPIOD->BSRRL = GPIO_Pin_12;
+//			Delay(5000000);
+//			GPIOD->BSRRH = GPIO_Pin_12;
+////Uruchomiony Timer
+//			TIM_Cmd(TIM2, ENABLE);
+////Oczekiwanie na sygnal ECHO
+//			if (!GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13)) {
+//				TIM_Cmd(TIM2, DISABLE);
+//
+//			} else if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13)) {
+////Odczytanie wartosci licznika i pomiar
+//				TIM2_CNT = TIM2->CNT;
+//				Distance = (float) TIM2_CNT / 58;
+//				TIM_Cmd(TIM2, DISABLE);
+//			}
+//
+//			TIM_ClearFlag(TIM4, TIM_FLAG_Update);
+//
+//		}
+//	}
 
-	for (;;) 
-	{
-		if (TIM_GetFlagStatus(TIM4, TIM_FLAG_Update))
-		{
-//Wyslany sygnal trigger
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
-//Uruchomiony Timer
-			TIM_Cmd(TIM2, ENABLE);
-//Oczekiwanie na sygnal ECHO
-			if (GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13)) 
-			{
-					TIM_Cmd(TIM2, DISABLE);
-
-			} else if (!GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_13)) 
-			{
-//Odczytanie wartosci licznika i pomiar
-					TIM2_CNT = TIM2->CNT;
-					Distance = (float) TIM2_CNT / 58;
-				//TIM_Cmd(TIM2, DISABLE);
-			}
-
-			TIM_ClearFlag(TIM4, TIM_FLAG_Update);
-
+	for (;;) {
+		GPIOD->BSRRH = GPIO_Pin_12;
+		Delay(1000000);
+		GPIOD->BSRRL = GPIO_Pin_12;
+		Delay(5000000);
+		GPIOD->BSRRH = GPIO_Pin_12;
+		while ((GPIOD->IDR && GPIO_Pin_13) == 0x00) {
+			a++;
 		}
+
+		TIM2_CNT = 0;
+		while ((GPIOD->IDR && GPIO_Pin_13) != 0x00) {
+			TIM2_CNT++;
+		}
+		TIM4_CNT = TIM2_CNT;
+		Distance = (float) TIM4_CNT * 0.17;
+		Delay(500000);
 	}
 }
